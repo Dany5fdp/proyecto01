0e7d172ac31aa2ac4e1f59e473a730bc2773987c
javiergzhdz@gmail.com <1Blueorchid@>
2019-03-11 15:12:16 -0600

    Initial Commit

---
+++ proyecto01/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  proyecto01
+//
+//  Created by Macbook on 3/11/19.
+//  Copyright © 2019 ioslab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
